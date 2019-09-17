41e1a2f57607d295e758b98e61e488ea758cd58d
szsed <szabolcs.sed@gmail.com>
2019-09-17 10:33:51 +0200

    init project

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+  devServer: {
+    contentBase: './dist',
+  },
+};
