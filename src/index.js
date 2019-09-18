8a46fdb10b1ca29f06991fa6565d8d9bb847bb66
szsed <szabolcs.sed@gmail.com>
2019-09-18 09:31:16 +0200

    add keyboard listeners

--- src/index.js
+++ src/index.js
@@ -28,6 +28,7 @@ class SimpleGoldenCornApp extends React.Component {
     this.state = { acorns: props.acorns }
     this.buyOne = this.buyOne.bind(this);
     this.eatOne = this.eatOne.bind(this);
+    this.handleKeyDown = this.handleKeyDown.bind(this);
   }
 
   buyOne() {
@@ -38,6 +39,22 @@ class SimpleGoldenCornApp extends React.Component {
     if (this.state.acorns > 0) {
       this.setState({ acorns: this.state.acorns - 1 });
     }
+  }
+
+  handleKeyDown(event) {
+    if (event.keyCode === 38) {
+      this.buyOne();
+    } else if (event.keyCode === 40) {
+      this.eatOne();
+    }
+  }
+
+  componentDidMount() {
+    document.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleKeyDown);
   }
 
   render() {
