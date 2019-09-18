b5ca5550d981715c26fe80db055a961e339752d8
szsed <szabolcs.sed@gmail.com>
2019-09-19 01:16:46 +0200

    add app with states to routes

---
+++ src/AppStates.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Button } from './components/Button';
+import { Display } from './components/Display';
+
+class AcornAppStates extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { acorns: props.acorns }
+    this.buyOne = this.buyOne.bind(this);
+    this.eatOne = this.eatOne.bind(this);
+    this.handleKeyDown = this.handleKeyDown.bind(this);
+  }
+
+  buyOne() {
+    this.setState({ acorns: this.state.acorns + 1 });
+  }
+
+  eatOne() {
+    if (this.state.acorns > 0) {
+      this.setState({ acorns: this.state.acorns - 1 });
+    }
+  }
+
+  handleKeyDown(event) {
+    if (event.keyCode === 38) {
+      this.buyOne();
+    } else if (event.keyCode === 40) {
+      this.eatOne();
+    }
+  }
+
+  componentDidMount() {
+    document.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  render() {
+    return (<>
+      <Button func={this.buyOne} str='Buy One' />
+      <Display>{this.state.acorns}</Display>
+      <Button func={this.eatOne} str='Eat One' />
+    </>
+    );
+  }
+}
+
+export default AcornAppStates;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AcornAppRedux from './AppRedux';
+import AcornAppStates from './AppStates';
 import store from './store';
 import { Provider } from 'react-redux';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
@@ -8,12 +9,13 @@ import { Navbar } from './components/Navbar';
 import { Home } from './Home';
 
 
+
 ReactDOM.render(<Provider store={store} >
   <BrowserRouter>
     <Navbar />
     <Switch>
       <Route exact path="/" component={Home} />
-      {/* <Route path="/simple/states" component={AcornAppStates} /> */}
+      <Route path="/simple/states" render={() => <AcornAppStates acorns={0} />} />
       <Route path="/simple/redux" component={AcornAppRedux} />
     </Switch>
   </BrowserRouter>
